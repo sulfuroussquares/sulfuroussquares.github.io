11a5dd78ee6a59c8d39ce1cdbda44a31d8b17635
sulfuroussquares <bbetancova@gmail.com>
2020-01-22 19:24:48 -0800

    Updates

---
+++ precache-manifest.6d931c00d4f23e60edebe52e5281ab81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a96057c19e48fabc7e13f033a38482",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "9a3ab5dfc858d4b9c84d",
+    "url": "/portfolio-page/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "0377fe654a4f7895ae04",
+    "url": "/portfolio-page/static/css/main.f9b6a095.chunk.css"
+  },
+  {
+    "revision": "9a3ab5dfc858d4b9c84d",
+    "url": "/portfolio-page/static/js/2.daaa27b9.chunk.js"
+  },
+  {
+    "revision": "f111f0e43b20013304c8bd0659a06b92",
+    "url": "/portfolio-page/static/js/2.daaa27b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0377fe654a4f7895ae04",
+    "url": "/portfolio-page/static/js/main.46f3c301.chunk.js"
+  },
+  {
+    "revision": "187d49440d7f87c34ef8",
+    "url": "/portfolio-page/static/js/runtime-main.9cd916a2.js"
+  },
+  {
+    "revision": "3969ffae6ffec082e683d5e3f964ca2a",
+    "url": "/portfolio-page/static/media/background.3969ffae.jpg"
+  }
+]);
