b57c674948665fd1ee178adbe032b085f501d246
sulfuroussquares <bbetancova@gmail.com>
2020-01-28 19:24:08 -0800

    Added Portfolio Page Build

---
+++ precache-manifest.2fe82bd46bfcf5246870a91131aca905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4c3e7fbfeca1a385237354fb217f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7573e07b49e539bcabf",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "19afd32fdbaecd5b09bb",
+    "url": "/static/css/main.f9b6a095.chunk.css"
+  },
+  {
+    "revision": "f7573e07b49e539bcabf",
+    "url": "/static/js/2.baf63ccd.chunk.js"
+  },
+  {
+    "revision": "f111f0e43b20013304c8bd0659a06b92",
+    "url": "/static/js/2.baf63ccd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19afd32fdbaecd5b09bb",
+    "url": "/static/js/main.517c7680.chunk.js"
+  },
+  {
+    "revision": "6957b8172fb5b5e8ff3e",
+    "url": "/static/js/runtime-main.c95c5758.js"
+  }
+]);
